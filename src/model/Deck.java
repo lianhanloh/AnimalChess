dad1e4c7618fbaa8df12d7098f9eb4e80db3e5c5
Lianhan Loh <loh.lianhan@gmail.com>
2014-04-22 10:45:05 -0400

    added Deck.java

---
+++ src/model/Deck.java
@@ -0,0 +1,26 @@
+package model;
+
+/**
+ * This object holds a deck of cards
+ * @author lianhanloh
+ *
+ */
+
+public class Deck {
+
+    /**
+     * class fields
+     */
+    private static Card[] deck;
+    
+    public Deck () {
+        
+    }
+    
+    /**
+     * shuffles deck
+     */
+    public static void shuffle () {
+        
+    }
+}
