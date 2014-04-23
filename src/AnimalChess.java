1a8a18196ca2c3501ea127a3f85ea21db0e1debf
Lianhan Loh <loh.lianhan@gmail.com>
2014-04-23 02:06:49 -0400

    Background displays, manipulating window size now

--- src/AnimalChess.java
+++ src/AnimalChess.java
@@ -10,14 +10,26 @@ import java.awt.event.*;
 
 import javax.swing.*;
 
+import view.BoardPanel;
+
 public class AnimalChess implements Runnable {
 
+    /** class fields */
+    private static final Dimension minDim = new Dimension(400,600);
+    
     @Override
     public void run() {
         
         // initialize frame and set location
         JFrame frame = new JFrame ("Animal Chess");
-        frame.setLocation(300, 300);
+//        frame.setLocation(300, 300);
+        frame.setLocationRelativeTo(null);
+        
+        frame.setMinimumSize(minDim);
+        
+        // add Panels
+        final BoardPanel board = new BoardPanel();
+        frame.add(board);
 
         // Put the frame on the screen
         frame.pack();
--- src/view/BoardPanel.java
+++ src/view/BoardPanel.java
@@ -1,5 +1,11 @@
 package view;
 
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
 import javax.swing.*;
 
 /**
@@ -12,4 +18,34 @@ import javax.swing.*;
 @SuppressWarnings("serial")
 public class BoardPanel extends JPanel {
 
+    private static final String bg_file = "images/dou_shou_qi_board.png";
+    private static int minHeight = 398;
+    private static int minWidth = 300;
+    private static int width;
+    private static int height;
+    private static final Dimension minimumSize = new Dimension(minWidth,
+            minHeight);
+    private static BufferedImage bg;
+
+    public BoardPanel() {
+        setMinimumSize(minimumSize);
+        try {
+            bg = ImageIO.read(new File(bg_file));
+        } catch (IOException e) {
+            System.out.println("Internal Error:" + e.getMessage());
+        }
+    }
+
+
+    public void paintComponent (Graphics g) {
+        width = getWidth();
+        height = getHeight();
+        super.paintComponent(g);
+        g.drawImage(bg, 0, 0, width, height, null);
+    }
+
+    @Override
+    public Dimension getPreferredSize() {
+        return new Dimension(minWidth, minHeight);
+    }
 }
