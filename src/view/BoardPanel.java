c4d44c486255df46b84bb73feedb09caa59c8044
Lianhan Loh <loh.lianhan@gmail.com>
2014-04-23 00:34:31 -0400

    game board added here (in view package)

---
+++ src/view/BoardPanel.java
@@ -0,0 +1,15 @@
+package view;
+
+import javax.swing.*;
+
+/**
+ * This class displays the state of the current board to the user
+ * 
+ * @author lianhanloh
+ *
+ */
+
+@SuppressWarnings("serial")
+public class BoardPanel extends JPanel {
+
+}
