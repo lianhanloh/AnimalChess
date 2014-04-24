4eff66dab5ae3ac71d0920196b0390639ceef886
Lianhan Loh <loh.lianhan@gmail.com>
2014-04-23 22:03:05 -0400

    deleted refactored lines from view

--- src/view/BoardPanel.java
+++ src/view/BoardPanel.java
@@ -94,40 +94,4 @@ public class BoardPanel extends JPanel {
         return squares;
     }
 
-//    /**
-//     * This method adds the mouse click listeners to each button
-//     */
-//    private static void addEventListeners () {
-//        for (int y = 0; y < squares[0].length; y++){
-//            for (int x = 0; x < squares.length; x++) {
-//                squares[x][y].addActionListener(new ActionListener() {
-//
-//                    @Override
-//                    public void actionPerformed(ActionEvent e) {
-//                        processSelection(e);
-//                    }                  
-//                });
-//            }
-//        }
-//    }
-//    
-//    /**
-//     * This method deals with the action listener received by a button
-//     * @param e the button's action
-//     */
-//    private static void processSelection(ActionEvent e) {
-//        Object source = e.getSource();
-//        for (int y = 0; y < squares[0].length; y++){
-//            for (int x = 0; x < squares.length; x++) {
-//                // de-select previously selected button
-//                squares[x][y].setSelected(false);
-//                // selects correct button
-//                if (squares[x][y] == source) {
-//                    squares[x][y].setSelected(true);
-//                }
-//                // repaint each button
-//                squares[x][y].repaint();
-//            }
-//        }
-//    }
 }
