0f96c6fcf3b0d9e51bab29c77e4391f3e5bfc3cf
Lianhan Loh <loh.lianhan@gmail.com>
2014-04-24 23:15:59 -0400

    refactored AnimalChess.java to Game.java because of HW submission instruction

--- src/Game.java
+++ src/Game.java
@@ -1,69 +1,45 @@
 /**
- * CIS 120 HW10
- * (c) University of Pennsylvania
- * @version 2.0, Mar 2013
+ * This is the main program class and runs the game
+ * 
+ * @author lianhanloh
+ *
  */
 
-// imports necessary libraries for Java swing
-import java.awt.*;
-import java.awt.event.*;
 import javax.swing.*;
 
-/**
- * Game Main class that specifies the frame and widgets of the GUI
- */
+import view.GUI;
+
 public class Game implements Runnable {
-	public void run() {
-		// NOTE : recall that the 'final' keyword notes inmutability
-		// even for local variables.
 
-		// Top-level frame in which game components live
-		// Be sure to change "TOP LEVEL FRAME" to the name of your game
-		final JFrame frame = new JFrame("TOP LEVEL FRAME");
-		frame.setLocation(300, 300);
+    /** class fields */
+    
+    @Override
+    public void run() {
+        
+        // initialize frame and set location
+        JFrame frame = new JFrame ("Animal Chess");
+        
+        // set up location and size
+        frame.setLocation(300, 100);
+        frame.setResizable(false);
+        
+        // add GUI
+        final GUI GUI = new GUI();
+        frame.add(GUI);
 
-		// Status panel
-		final JPanel status_panel = new JPanel();
-		frame.add(status_panel, BorderLayout.SOUTH);
-		final JLabel status = new JLabel("Running...");
-		status_panel.add(status);
+        // Put the frame on the screen
+        frame.pack();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+    }
 
-		// Main playing area
-		final GameCourt court = new GameCourt(status);
-		frame.add(court, BorderLayout.CENTER);
+    /**
+     * main method to start and run the game. No arguments necessary
+     * 
+     * @param args
+     */
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Game());
+    }
 
-		// Reset button
-		final JPanel control_panel = new JPanel();
-		frame.add(control_panel, BorderLayout.NORTH);
-
-		// Note here that when we add an action listener to the reset
-		// button, we define it as an anonymous inner class that is
-		// an instance of ActionListener with its actionPerformed()
-		// method overridden. When the button is pressed,
-		// actionPerformed() will be called.
-		final JButton reset = new JButton("Reset");
-		reset.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				court.reset();
-			}
-		});
-		control_panel.add(reset);
-
-		// Put the frame on the screen
-		frame.pack();
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setVisible(true);
-
-		// Start game
-		court.reset();
-	}
-
-	/*
-	 * Main method run to start and run the game Initializes the GUI elements
-	 * specified in Game and runs it IMPORTANT: Do NOT delete! You MUST include
-	 * this in the final submission of your game.
-	 */
-	public static void main(String[] args) {
-		SwingUtilities.invokeLater(new Game());
-	}
 }
